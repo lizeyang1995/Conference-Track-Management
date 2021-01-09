249aec65bb13d3f138e959a2ca33feedc7c0595b
lizeyang1995 <424011527@qq.com>
2021-01-09 16:45:49 +0800

    [lizeyang] feat:  create Track

--- src/Service.kt
+++ src/Service.kt
@@ -1,5 +1,4 @@
 fun parseTalk(talk: String): TalkAndTimes {
-    //Writing Fast Tests Against Enterprise Rails 60min
     val splitedTalk = talk.split(" ")
     val times = splitedTalk[splitedTalk.size - 1].substringBefore("min")
     val talkAndTimes = TalkAndTimes(talk, times.toInt())
---
+++ src/Track.kt
@@ -0,0 +1,4 @@
+class Track {
+    lateinit var morningSession: MorningSession
+    lateinit var afternoonSession: AfternoonSession
+}
