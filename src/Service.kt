8220a4ab57d015a902f38a3ed0f68c771d0e06fa
lizeyang1995 <424011527@qq.com>
2021-01-09 18:45:07 +0800

    [lizeyang] feat: add a function named parseTime

--- src/Service.kt
+++ src/Service.kt
@@ -15,7 +15,7 @@ fun assignTalks(talks: MutableList<TalkAndTimes>): MutableList<Track> {
     var afternoonSession = AfternoonSession()
     var morningRemainingTime = morningSession.RemainingTime
     var afternoonRemainingTime = afternoonSession.RemainingTime
-    var tracks = mutableListOf<Track>()
+    val tracks = mutableListOf<Track>()
     for (talk in sortedTalks) {
         if (morningRemainingTime - talk.times >= 0) {
             morningRemainingTime -= talk.times
@@ -38,4 +38,37 @@ fun assignTalks(talks: MutableList<TalkAndTimes>): MutableList<Track> {
     track.afternoonSession = afternoonSession
     tracks.add(track)
     return tracks
+}
+
+fun parseTime(tracks: MutableList<Track>) {
+    for (index in tracks.indices) {
+        println("Track ${index + 1}:")
+        var hour: Int
+        var minute: Int
+        var morningTimeConsumed = 0
+        val morningStartTime = 9
+        for (talkAndTimes in tracks[index].morningSession.selectedtalks) {
+            hour = morningTimeConsumed / 60
+            morningTimeConsumed += talkAndTimes.times
+            minute = (morningTimeConsumed - talkAndTimes.times) % 60
+            val time = String.format("%02d:%02d", morningStartTime + hour, minute)
+            println(time + "AM ${talkAndTimes.talk}")
+        }
+        println("12:00PM Lunch")
+        var afternoonTimeConsumed = 0
+        val afternoonStartTime = 1
+        for (talkAndTimes in tracks[index].afternoonSession.selectedtalks) {
+            hour = afternoonTimeConsumed / 60
+            afternoonTimeConsumed += talkAndTimes.times
+            minute = (afternoonTimeConsumed - talkAndTimes.times) % 60
+            val time = String.format("%02d:%02d", afternoonStartTime + hour, minute)
+            println(time + "PM ${talkAndTimes.talk}")
+        }
+        if (afternoonTimeConsumed <= 180) {
+            println("04:00PM Networking Event")
+        } else {
+            println("05:00PM Networking Event")
+        }
+        println("\n")
+    }
 }
---
+++ src/main.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main() {
+    val talks = Talks("/Users/zeyang.li/Documents/Kotlin_test/Conference Track Management/src/Talks.txt").obtainTalks()
+    val parsedTalks = mutableListOf<TalkAndTimes>()
+    for (talk in talks) {
+        val parsedTalk = parseTalk(talk)
+        parsedTalks.add(parsedTalk)
+    }
+    val tracks = assignTalks(parsedTalks)
+    parseTime(tracks)
+}
