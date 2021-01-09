546f0ca37b1d8aae7596ef34b9ae8ba8efb33c61
lizeyang1995 <424011527@qq.com>
2021-01-09 20:40:45 +0800

    [lizeyang] feat: modify file path

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,7 @@
 import java.util.*
 
 fun main() {
-    val talks = Talks("/Users/zeyang.li/Documents/Kotlin_test/Conference Track Management/src/Talks.txt").obtainTalks()
+    val talks = Talks("/Users/zeyang.li/Documents/Kotlin_test/Conference-Track-Management/src/Talks.txt").obtainTalks()
     val parsedTalks = mutableListOf<TalkAndTimes>()
     for (talk in talks) {
         val parsedTalk = parseTalk(talk)
